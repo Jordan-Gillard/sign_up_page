c223cf5c48a1b210a5352c5ff2991ae116732489
Jordan Gillard <jordan-gillard@outlook.com>
2020-07-09 20:03:03 -0500

    refactor: Removed Center widget around login page column

--- lib/login.dart
+++ lib/login.dart
@@ -6,82 +6,80 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.centerLeft,
-              colors: [blueMaterialColor.shade50, blueMaterialColor.shade300],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.centerLeft,
+            colors: [blueMaterialColor.shade50, blueMaterialColor.shade300],
           ),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 45,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 70.0, 40.0, 40.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.account_circle,
-                        color: iconTurquoise,
-                        size: 80.0,
-                      ),
-                      Spacer(),
-                      Text("Start managing your",
-                          style: largeTextStyle
-                              .merge(TextStyle(color: lightTextGray))),
-                      Text("money with us!",
-                          style: largeTextStyle
-                              .merge(TextStyle(fontWeight: FontWeight.bold))),
-                      Spacer(),
-                      Row(
-                        children: <Widget>[
-                          RaisedButton(
-                            shape: defaultRectangularButtonShape,
-                            padding: EdgeInsets.symmetric(
-                                vertical: 15.0, horizontal: 20.0),
-                            color: blueMaterialColor.shade50,
-                            onPressed: () {
-                              print("Log in button pressed.");
-                            },
-                            child: Row(
-                              children: <Widget>[
-                                Text("Already signed up? ",
-                                    style: smallTextStyle.merge(
-                                        TextStyle(color: lightTextGray))),
-                                Text(
-                                  "Log in",
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 45,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 70.0, 40.0, 40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(
+                      Icons.account_circle,
+                      color: iconTurquoise,
+                      size: 80.0,
+                    ),
+                    Spacer(),
+                    Text("Start managing your",
+                        style: largeTextStyle
+                            .merge(TextStyle(color: lightTextGray))),
+                    Text("money with us!",
+                        style: largeTextStyle
+                            .merge(TextStyle(fontWeight: FontWeight.bold))),
+                    Spacer(),
+                    Row(
+                      children: <Widget>[
+                        RaisedButton(
+                          shape: defaultRectangularButtonShape,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 20.0),
+                          color: blueMaterialColor.shade50,
+                          onPressed: () {
+                            print("Log in button pressed.");
+                          },
+                          child: Row(
+                            children: <Widget>[
+                              Text("Already signed up? ",
                                   style: smallTextStyle.merge(
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                                )
-                              ],
-                            ),
+                                      TextStyle(color: lightTextGray))),
+                              Text(
+                                "Log in",
+                                style: smallTextStyle.merge(
+                                    TextStyle(fontWeight: FontWeight.bold)),
+                              )
+                            ],
                           ),
-                          Spacer()
-                        ],
-                      )
-                    ],
-                  ),
+                        ),
+                        Spacer()
+                      ],
+                    )
+                  ],
                 ),
               ),
-              Expanded(
-                flex: 55,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius:
-                          BorderRadius.only(topRight: Radius.circular(70.0))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: CreateAccountForm(),
-                  )
-                ),
-              )
-            ],
-          ),
+            ),
+            Expanded(
+              flex: 55,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(topRight: Radius.circular(70.0))),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: CreateAccountForm(),
+                )
+              ),
+            )
+          ],
         ),
       ),
     );
